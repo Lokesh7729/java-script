02884eeae54b7a7c2236fbd7b78efc126fa5a9ab
Lokesh Chauhan <126611880+Lokesh7729@users.noreply.github.com>
2023-08-29 12:17:51 +0000

    Conversion of DataTypes

---
+++ 01_basics/03_conversionOperaions.js
@@ -0,0 +1,27 @@
+let score = true
+//console.log(typeof score);
+//console.log(typeof (score));
+
+//let valueInNumber = Number(score);
+//console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+
+//  "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+// "" => false
+// "lokesh" => true
+
+let isLoggedIn = "lokesh";
+let booleanIsLoogedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoogedIn);
+
+
+
+let someNumber= 34
+let stringNumber= String(someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
+
+
+
