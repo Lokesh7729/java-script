8ed63d30dd189d92421a475272e4c57d1cdd66d2
Lokesh Chauhan <126611880+Lokesh7729@users.noreply.github.com>
2023-08-29 15:50:10 +0000

    datatypes - summary

---
+++ datatypes-summary.js
@@ -0,0 +1,40 @@
+// Primitive 
+
+// 7 types : String ,Number , Boolearn , null , undefined, Symbol (used to make component unique), BigInt
+
+// --- 
+
+const score = 100 
+const scoreValue = 100.3
+
+const isLonggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId =Symbol('123');
+console.log(id === anotherId); // not same because they have unique id
+
+const bigNumber = 2649363949236062n
+
+const heros = ["shaktian", "naagraj","doga"];
+
+let myObj = {
+    name : "lokesh",
+    age: 20,
+}
+
+const myFunction = function(){
+    console.log("hello world")
+}
+
+console.log(typeof scoreValue);
+
+//Reference (NOn primitive)
+
+// Array, Objects, Functions
+
+
+
+//NULL => Object
+
