5f9130cdf4923744fa2c9787aa2a904ed2c63aa2
Lokesh Chauhan <126611880+Lokesh7729@users.noreply.github.com>
2023-08-29 12:01:58 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3) // we are using nodejs , not browser
+
+console.log(3+3)
+console.log("lokesh")
+
+// number => 1 to power 53
+// bigint
+// string = ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// Symbol => unique
+
+let age = 19
+
+// Object 
+
+console.log(typeof age)
+console.log(typeof null) 
