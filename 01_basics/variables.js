08a311aa06bb9d3249ccf5109a7fb4002cb8b8fd
Lokesh Chauhan <126611880+Lokesh7729@users.noreply.github.com>
2023-08-29 11:27:03 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,12 @@
+const account = 14435
+let accounntEmail  ="lokesh@gmail.com"
+var accountPassword= "1234"
+accountCity = "indore"
+let marks
+
+
+// prefer not to use var
+// because of issue in block scope and fucnctional scope
+
+
+console.table([account, accounntEmail,accountPassword,accountCity,marks]);
