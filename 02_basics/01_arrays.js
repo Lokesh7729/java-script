2e2aee9982803a24d3f0b40735646d4ff7f16b79
Lokesh Chauhan <126611880+Lokesh7729@users.noreply.github.com>
2023-09-06 12:37:24 +0000

    arrays basic

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,35 @@
+// array
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeors = ["shaktiman","naagraj"]
+
+
+
+
+console.log(myArr[5]);
+console.log(myHeors[0]);
+
+// arrays methods 
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.indexOf(5));
+// console.log(myArr)
+
+
+// slice , splice
+console.log("A", myArr);
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("b", myArr);
+
+// slice returns a piece of the array but it doesn't affect the original array.
+//  splice changes the original array by removing, replacing, or adding values and returns the affected values
+
+
